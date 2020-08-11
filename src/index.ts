6e8e517f47d179f5eeef1761d670870aa557c7a6
Jonny <jmwright@scottlogic.com>
2020-08-11 09:24:48 +0100

    Basic express end point listening

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world!!");
+import Express from 'express'
+
+const PORT = 3000
+
+const app = Express()
+
+app.get('/internal/healthcheck', (_req, res) => {
+  res.status(200).send('success')
+})
+
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`))
