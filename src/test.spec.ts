886672d45af026095f395eb30bb935698932ea0a
Jonny <jmwright@scottlogic.com>
2020-08-11 11:48:17 +0100

    Add unit tests

---
+++ src/test.spec.ts
@@ -0,0 +1,7 @@
+describe('unit test placeholder', () => {
+  it('should run a test', () => {
+    const actual = 'hello' + ' ' + 'world'
+    const expected = 'hello world'
+    expect(actual).toEqual(expected)
+  })
+})
