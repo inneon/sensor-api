02404566694b795660629a457c98bb6c9ab99bfa
Jonny <jmwright@scottlogic.com>
2020-08-15 21:59:55 +0100

    Tidy up

--- src/test.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('unit test placeholder', () => {
-  it('should run a test', () => {
-    const actual = 'hello' + ' ' + 'world'
-    const expected = 'hello world'
-    expect(actual).toEqual(expected)
-  })
-})
